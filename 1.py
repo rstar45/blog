388423ffa2de72a517609b2909bc7f7dd8fa5a35
rstar45 <168097347+rstar45@users.noreply.github.com>
2024-04-25 20:58:53 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,236 @@
+#name = ("mark")
+#print(f" my name is {name} ")
+
+#print("I like apple")
+#print("I donot like tea")
+
+#gpt=3.3
+#print(f" my gpt {gpt} marks")
+#print(type (gpt))
+
+#money = 9
+#print("my money + str(money) ddooa")
+#print(type (money))
+
+#oneys = 509.5
+#rint(f"# my money {moneys} $hk")
+#print(type (moneys))
+
+#name = "CC"
+#print(type (name))
+#print(f" my name is {name} ")
+
+
+#is_online = True
+#print(f"Are you online?{is_online}")
+#rint(type (is_online))
+
+#no_online = False
+#print(f"Ar eyou online?{no_online}")
+#print(type (no_online))
+
+#dj_2 = input("Writting the second adj:")
+#verb = input("Writting the verb:")
+#adj_3 = input("Writting the third adj:")
+
+#print(f"Today,I go to a {adj_1} park,there are {adj_2}.There's weather is {adj_3},I {verb} fast while my mum was tell me to take a {noun} to home.")
+
+#length = float (input("Writting the length of the rectangle"))
+#width = float (input("Writting the width of the rectangle"))
+
+
+
+#area = length * width
+#print(f"The area is {area}m2")
+
+#adj_1 = input("Writting the first adj")
+#adj_3 = input("Writting the third adj")
+#noun = input("Writting the noun")
+#verb = input("Writtng the verb")
+
+#print(f"Yesterday,I went to the ocean park,there were {adj_1}.I {verb} up the flower and smaled it.I felt {adj_2}.Suddenly a {noun} shouted,'chirp'I felt {adj_3}")
+
+#choose_1 = input("Are you doing homework")
+
+#yes = True
+#no = False
+
+#print (yes)
+
+#name = input("Wwriting your name:")
+#wd aprint(f"your name is {name}")
+
+#length = float(input("Writing the length of the rectangle:"))
+#worth = float(input("Writing the worth of the rectangle:"))
+#rea = float(input(length * worth))
+#print(f"The rectanglef area is {area}m" )
+
+#item = input("Which one would you like?")
+#price = float(input("What is the price?"))
+#quantity = int(input("How many would you like?"))
+#total = price * quantity
+#print(f"You bought {quantity} {item},it is ${total}")
+
+#money = 9
+#allmoney = money * 3
+#allmoney *= 8
+
+#print(allmoney)
+
+#allmoney = money / 3
+#print(allmoney)
+#numbers_1 = float(input(("  "))
+#nmbers_2 = float(input("  "))
+#numbers = numbers_1 * numbers_2
+#print(numbers)
+
+#name = input("what is your name:")
+#print(f"your name is {name}")
+#购物车：
+#price = float(input("how much?"))
+#quantity = float(input("how many would you like"))
+#total = price * quantity
+#print(f"the total is {total}")
+
+#算乘法：
+#numbers_1 = float(input(" "))
+#umbers_2 = float(input(" *"))
+#numbers = numbers_1 * numbers_2
+#print(f"= {numbers}")
+
+#算次方：
+#umbers_1 = float(input(" "))
+#numbers_2 = float(input(" **"))
+#numbers = numbers_1 ** numbers_2
+#print(f"= {numbers}")
+
+#算余数：
+#numbers_1 = float(input(" "))
+#numbers_2 =float(input("  %"))
+#numbers = numbers_1 % numbers_2
+#print(f"= {numbers}")
+
+#函数：
+#函数次方：
+#numbers_1 = float(input(" "))
+#numbers_2 = float(input(" "))
+#print(pow(numbers_1,numbers_2))
+
+#函数最大值与最小值
+#numbers_1 = float(input(" "))
+#numbers_2 = float(input(" "))
+#numbers_3 = float(input(" "))
+#print(max(numbers_1,numbers_2,numbers_3))
+#print(min(numbers_1,numbers_2,numbers_3))
+
+#四舍五入
+#numbers_1 = float(input(" "))
+#print(round(numbers_1))
+
+#绝对值：
+#numbers_1 = float(input(" "))
+#print("绝对值:",abs(numbers_1) )
+
+#四舍五入 无条件进位
+#import math
+#numbers_1 = float(input(" "))
+#print(math.ceil(numbers_1))
+
+#四舍五入 无条件舍去
+#import math
+#numbers_1 = float(input(" "))
+#print(math.floor(numbers_1))
+
+#圆周率
+#import math
+#print(math.pi)
+
+#圆的周长
+#import math
+#numbers_2 = (math.pi)
+#numbers_1 = float(input("圆的半径: "))
+#numbers = numbers_1 * numbers_2 * 2
+#print(f"圆的周长是：{round(numbers,2)}")
+
+#圆的面积
+#import math
+#numbers_2 = (math.pi)
+#numbers_1 = float(input("圆的半径: "))
+#numbers = numbers_2 * (numbers_1 ** 2)
+#print(f"圆的面积是：{round(numbers,2)}")
+
+#if and if else
+#input("Have the goods sold?")
+#sold = True
+#if sold:
+ #      print("The goods have sold")
+
+#else:
+#    print("the goods have not sold")
+
+#if else
+#age = int(input("Writing your ago"))
+#if age >=100:
+#              print("you are so old,you can't sign up")
+#elif age >= 18:
+ #           print("you can sign up ")
+#elif age <=0:
+#            print("you have not born")
+#else:
+#    print("you can't sigh up,you must be older than 18 years")
+
+#age = int(input("what is your age"))
+#if age >= 100:
+ #            print("you age is so old，you can't sign up")
+
+#elif age <= 0:
+ #            print("you have not born")
+
+#elif age < 18:
+ #          print("you can't sign up,you must be 18 year ")
+
+#else:
+#    print("you can sign up")
+
+#简易计算机
+#operator = input("please writing the operator:加法：+，减法：-，乘法：*，除法：/,次方：**:")
+#numbers_1 = float(input("please writing the first number: "))
+#numbers_2 = float(input("please writing the second number or power numbers: "))
+
+#if operator == "+":
+ #               result = numbers_1 + numbers_2
+#elif operator == "-":
+ #                 result = numbers_1 - numbers_2
+#elif operator == "*":
+ #               result = numbers_1 * numbers_2
+ #elif operator == "/":
+#                result = numbers_1 / numbers_2
+#elif operator == "**":
+#    result = numbers_1 ** numbers_2
+#else:
+#     print("the operator can't use")
+
+#print(f"the result is {round(result,2)}")
+
+#体重转化器
+#weight = float(input("please writing your weight: "))
+#unit = input("your weight are kg or g or pound:(kg,g,lb)")
+
+#if unit =="kg":
+#    weight *= 2.2
+#    new_unit = "LB"
+
+#elif unit =="lb":
+#    weight /= 2.2
+#    new_unit = "KG"
+#    weight = round(weight, 2)
+
+
+#elif unit =="g":
+#    weight *= 0.001
+#    new_unit = "KG"
+
+#else:
+#     print("The unit is fales")
+
+#print(f"your weight is {weight} {new_unit}")
